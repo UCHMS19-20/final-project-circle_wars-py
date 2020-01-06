b9f7a986dfff0ba1e462a431ac49adebaf92f1cb
KenTheNoob <32941731+KenTheNoob@users.noreply.github.com>
2020-01-06 11:09:19 -0500

    Update game.py

--- src/game.py
+++ src/game.py
@@ -5,13 +5,16 @@ pygame.init()
 width = 800
 height = 600
 
+stage_y = height/1.25
+stage_x = width/8
+
 character = {
     "diameter_x": width/25,
     "diameter_y": height/25,
-    "x": (width/8),
-    "y": (height/1.25)-(height/25),
+    "x": (stage_x),
+    "y": (stage_y)-(height/25),
     "increment": width/150,
-    "y_acceleration": -height/60,
+    "y_acceleration": -height/50,
     "y_velocity": 0,
     "x_velocity": width/150,
     "jumping": 0,
@@ -36,22 +39,17 @@ while True:
         height = event.h
         character["diameter_x"] = width/25
         character["diameter_y"] = height/25
-        character["y_acceleration"] = height/60
+        character["y_acceleration"] = height/50
         character["x_velocity"] = width/150
     
     keys = pygame.key.get_pressed()
-    
-    if keys[pygame.K_LEFT]:
-        character["x"] -= character["x_velocity"]
-
-    if keys[pygame.K_RIGHT]:
-        character["x"] += character["x_velocity"]
 
     if keys[pygame.K_DOWN]:
-        character["y_acceleration"] -= height/60
+        character["y_acceleration"] -= height/50
     
     if keys[pygame.K_UP] and character["jumping"] == 0 and character["jumps"] > 0:
         character["y_velocity"] = height/2.5
+        character["y_acceleration"] = -height/50
         character["jumping"] = 1
     
     if keys[pygame.K_UP] == False and character["jumping"] == 1:
@@ -59,23 +57,51 @@ while True:
         character["jumps"] -= 1
 
     character["y_velocity"] += character["y_acceleration"]
-    character["y"] -= round(character["y_velocity"]/25)
-    if character["x"] > width*0.125 - character["diameter_x"] and character["x"] < width*0.875:
-        if character["y"] >= (height/1.25)-(height/25):
-            character["y"] = (height/1.25)-(height/25)
+    
+    if character["x"] > stage_x - character["diameter_x"] and character["x"] < width*0.875:
+        if character["y"] - character["y_velocity"]/20 >= (stage_y)-(height/25) and character["y"] <= (stage_y)-(height/25):
+            character["y"] = (stage_y)-(height/25)
             character["y_velocity"] = 0
-            character["y_acceleration"] = -height/60
+            character["y_acceleration"] = -height/50
             character["jumps"] = 2
             character["jumping"] = 0
     else:
         if character["y"] >= height:
             character["y"] = height
             character["y_velocity"] = 0
-            character["y_aceleration"] = -height/60
+            character["y_aceleration"] = -height/50
             character["jumps"] = 2
             character["jumping"] = 0
+
+    character["y"] -= round(character["y_velocity"]/20)
+
+    if keys[pygame.K_LEFT]:
+        if character["x"] > stage_x - character["diameter_x"] and character["x"] < width*0.875:
+            character["x"] -= character["x_velocity"]
+        else:
+            if character["y"] + character["diameter_y"] <= stage_y:
+                character["x"] -= character["x_velocity"]
+            if character["y"] + character["diameter_y"] > stage_y:
+                if character["x"] - character["x_velocity"] > width*0.875:
+                    character["x"] -= character["x_velocity"]
+                elif character["x"] - character["diameter_x"] >= stage_x:
+                    character["x"] = width*0.875
     
-    pygame.draw.rect(win, (0, 0, 0), [width/8, height/1.25, width*0.75, height/5])
+    if keys[pygame.K_RIGHT]:
+        if character["x"] > stage_x - character["diameter_x"] and character["x"] < width*0.875:
+            character["x"] += character["x_velocity"]
+        else:
+            if character["y"] + character["diameter_y"] <= stage_y:
+                character["x"] += character["x_velocity"]
+            if character["y"] + character["diameter_y"] > stage_y:
+                if character["x"] + character["x_velocity"] < stage_x - character["diameter_x"]:
+                    character["x"] += character["x_velocity"]
+                elif character["x"] < width*0.875:
+                    character["x"] = stage_x - character["diameter_x"]
+
+
+
+    pygame.draw.rect(win, (0, 0, 0), [stage_x, stage_y, width*0.75, height/5])
     pygame.draw.ellipse(win, (0, 0, 255), (character["x"], character["y"], character["diameter_x"], character["diameter_y"]), width == 0)
     
     font = pygame.font.SysFont("Times New Roman", round(width/10))
