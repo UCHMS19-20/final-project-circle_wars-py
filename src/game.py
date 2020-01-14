528298fbcb44aaa0d3a54bc94236c35234a003d5
KenTheNoob <32941731+KenTheNoob@users.noreply.github.com>
2020-01-14 10:59:13 -0500

    Deleted old version of game and renamed new version. Added turnaround animations, a new weapon, and fixed a bug with window resize on start screen not changing the character and stage dimensions

--- src/game.py
+++ src/game.py
@@ -1,111 +1,170 @@
 import pygame
+import math
 
 pygame.init()
 
-width = 800
-height = 600
+screen_width = 800
+screen_height = 600
+stage_y = screen_height/1.25
+stage_x = screen_width/8
 
-stage_y = height/1.25
-stage_x = width/8
+class character:
+    def __init__(self, x_diameter, y_diameter, x_pos, y_pos, x_vel, x_accel, y_vel, y_accel,jumping, jumps, hitbox):
+        self.x_diameter = x_diameter
+        self.y_diameter = y_diameter
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.x_vel = x_vel
+        self.x_accel = x_accel
+        self.y_vel = y_vel
+        self.y_accel = y_accel
+        self.jumping = jumping
+        self.jumps = jumps
+        self.hitbox = pygame.Rect(self.x_pos, self.y_pos, self.x_diameter, self.y_diameter)
 
-character = {
-    "diameter_x": width/25,
-    "diameter_y": height/25,
-    "x": (stage_x),
-    "y": (stage_y)-(height/25),
-    "increment": width/150,
-    "y_acceleration": -height/50,
-    "y_velocity": 0,
-    "x_velocity": width/150,
-    "jumping": 0,
-    "jumps" : 2
-}
+p1 = character(screen_width/10, screen_height/10, stage_x, stage_y-screen_height/10, 0, 0, 0, -screen_height/50, 0, 2, 0)
 
-win = pygame.display.set_mode((width,height), pygame.RESIZABLE)
+win = pygame.display.set_mode((screen_width,screen_height), pygame.RESIZABLE)
 
-while True:
+p1_dead = False
+p1_face_right = True
+p1_face_left = False
+
+p1_sprite = pygame.image.load(r"C:\Users\kcheng\Documents\GitHub\final-project-circle_wars-py\src\img\reaper.png")
+p1_weapon = pygame.image.load(r"C:\Users\kcheng\Documents\GitHub\final-project-circle_wars-py\src\img\scythe.png")
+start_screen = pygame.image.load(r"C:\Users\kcheng\Documents\GitHub\final-project-circle_wars-py\src\img\StartScreen.png")
+p1_weapon = pygame.transform.rotozoom(p1_weapon, -45, 1)
+p1_weapon_x = p1.x_pos + p1.x_diameter*0.75
+
+game_start = False
+
+while game_start == False:
     pygame.time.delay(25)
     win.fill((255, 255, 255))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+            game_start = True
+        if event.type == pygame.VIDEORESIZE:
+            surface = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+            p1.x_pos = p1.x_pos * event.w/screen_width
+            p1.y_pos = p1.y_pos * event.h/screen_height
+            screen_width = event.w
+            screen_height = event.h
+            p1.x_diameter = screen_width/10
+            p1.y_diameter = screen_height/10
+            p1.y_accel = -screen_height/50
+            p1.x_vel = screen_width/150
+            p1.hitbox = pygame.Rect(p1.x_pos, p1.y_pos, p1.x_diameter, p1.y_diameter)
+            stage_y = screen_height/1.25
+            stage_x = screen_width/8
     
-    if event.type == pygame.VIDEORESIZE:
-        surface = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-        character["x"] = character["x"] * event.w/width
-        character["y"] = character["y"] * event.h/height
-        width = event.w
-        height = event.h
-        character["diameter_x"] = width/25
-        character["diameter_y"] = height/25
-        character["y_acceleration"] = height/50
-        character["x_velocity"] = width/150
-    
+    win.blit(pygame.transform.scale(start_screen, (screen_width, screen_height)), (0, 0))
+    pygame.display.update()
+
+while p1_dead == False:
+    pygame.time.delay(25)
+    win.fill((255, 255, 255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        elif event.type == pygame.VIDEORESIZE:
+            surface = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+            p1.x_pos = p1.x_pos * event.w/screen_width
+            p1.y_pos = p1.y_pos * event.h/screen_height
+            screen_width = event.w
+            screen_height = event.h
+            p1.x_diameter = screen_width/10
+            p1.y_diameter = screen_height/10
+            p1.y_accel = -screen_height/50
+            p1.x_vel = screen_width/150
+            p1.hitbox = pygame.Rect(p1.x_pos, p1.y_pos, p1.x_diameter, p1.y_diameter)
+            stage_y = screen_height/1.25
+            stage_x = screen_width/8
+
+    stage_hitbox = pygame.draw.rect(win, (0, 0, 0), [stage_x, stage_y, screen_width*0.75, screen_height/5])
+
     keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_r]:
+        screen_width = 800
+        screen_height = 600
+        stage_y = screen_height/1.25
+        stage_x = screen_width/8
+        p1 = character(screen_width/10, screen_height/10, stage_x, stage_y-screen_height/10, 0, 0, 0, -screen_height/50, 0, 2, 0)
+        win = pygame.display.set_mode((screen_width,screen_height), pygame.RESIZABLE)
+        p1_dead = False
 
     if keys[pygame.K_DOWN]:
-        character["y_acceleration"] -= height/50
+        p1.y_accel -= screen_height/50
     
-    if keys[pygame.K_UP] and character["jumping"] == 0 and character["jumps"] > 0:
-        character["y_velocity"] = height/2.5
-        character["y_acceleration"] = -height/50
-        character["jumping"] = 1
+    if keys[pygame.K_UP] and p1.jumping == 0 and p1.jumps > 0:
+        p1.y_vel = screen_height/2.5
+        p1.y_accel = -screen_height/50
+        p1.jumping = 1
     
-    if keys[pygame.K_UP] == False and character["jumping"] == 1:
-        character["jumping"] = 0
-        character["jumps"] -= 1
+    if keys[pygame.K_UP] == False and p1.jumping == 1:
+        p1.jumping = 0
+        p1.jumps -= 1
+    
+    p1.y_vel += p1.y_accel
+    p1.y_pos -= p1.y_vel/20
+    p1.hitbox = pygame.Rect(p1.x_pos, p1.y_pos, p1.x_diameter, p1.y_diameter)
 
-    character["y_velocity"] += character["y_acceleration"]
-    
-    if character["x"] > stage_x - character["diameter_x"] and character["x"] < width*0.875:
-        if character["y"] - character["y_velocity"]/20 >= (stage_y)-(height/25) and character["y"] <= (stage_y)-(height/25):
-            character["y"] = (stage_y)-(height/25)
-            character["y_velocity"] = 0
-            character["y_acceleration"] = -height/50
-            character["jumps"] = 2
-            character["jumping"] = 0
-    else:
-        if character["y"] >= height:
-            character["y"] = height
-            character["y_velocity"] = 0
-            character["y_aceleration"] = -height/50
-            character["jumps"] = 2
-            character["jumping"] = 0
-
-    character["y"] -= round(character["y_velocity"]/20)
+    if p1.hitbox.colliderect(stage_hitbox):
+        if p1.x_pos > stage_x - p1.x_diameter and p1.x_pos < screen_width*0.875:
+            p1.y_pos = stage_y - p1.y_diameter + 1
+            p1.y_vel = 0
+            p1.y_accel = -screen_height/50
+            p1.jumps = 2
+            p1.jumping = 0
 
     if keys[pygame.K_LEFT]:
-        if character["x"] > stage_x - character["diameter_x"] and character["x"] < width*0.875:
-            character["x"] -= character["x_velocity"]
-        else:
-            if character["y"] + character["diameter_y"] <= stage_y:
-                character["x"] -= character["x_velocity"]
-            if character["y"] + character["diameter_y"] > stage_y:
-                if character["x"] - character["x_velocity"] > width*0.875:
-                    character["x"] -= character["x_velocity"]
-                elif character["x"] - character["diameter_x"] >= stage_x:
-                    character["x"] = width*0.875
-    
+        if p1_face_right == True:
+            p1_sprite = pygame.transform.flip(p1_sprite, 1, 0)
+            p1_weapon = pygame.transform.flip(p1_weapon, 1, 0)
+            p1_face_right = False
+            p1_face_left = True
+        p1_weapon_x = p1.x_pos - p1.x_diameter*0.25
+        p1.x_vel = screen_width/150
+        p1.x_pos -= p1.x_vel
+    else:
+        p1.x_vel = 0
+        
     if keys[pygame.K_RIGHT]:
-        if character["x"] > stage_x - character["diameter_x"] and character["x"] < width*0.875:
-            character["x"] += character["x_velocity"]
-        else:
-            if character["y"] + character["diameter_y"] <= stage_y:
-                character["x"] += character["x_velocity"]
-            if character["y"] + character["diameter_y"] > stage_y:
-                if character["x"] + character["x_velocity"] < stage_x - character["diameter_x"]:
-                    character["x"] += character["x_velocity"]
-                elif character["x"] < width*0.875:
-                    character["x"] = stage_x - character["diameter_x"]
+        if p1_face_left == True:
+            p1_sprite = pygame.transform.flip(p1_sprite, 1, 0)
+            p1_weapon = pygame.transform.flip(p1_weapon, 1, 0)
+            p1_face_left = False
+            p1_face_right = True
+        p1_weapon_x = p1.x_pos + p1.x_diameter*0.75
+        p1.x_vel = screen_width/150
+        p1.x_pos += p1.x_vel
+    else:
+        p1.x_vel = 0
 
-
-
-    pygame.draw.rect(win, (0, 0, 0), [stage_x, stage_y, width*0.75, height/5])
-    pygame.draw.ellipse(win, (0, 0, 255), (character["x"], character["y"], character["diameter_x"], character["diameter_y"]), width == 0)
+    p1.y_pos -= 2
+    p1.hitbox = pygame.Rect(p1.x_pos, p1.y_pos, p1.x_diameter, p1.y_diameter)
     
-    font = pygame.font.SysFont("Times New Roman", round(width/10))
-    if character["x"] <= -character["diameter_x"] or character["x"] >= width or character["y"] <= -character["diameter_y"] or character["y"] >= height:
+    if p1.hitbox.colliderect(stage_hitbox):
+        if keys[pygame.K_LEFT]:
+            if p1.y_pos > (stage_y)-(screen_height/10):
+                p1.x_pos = screen_width*0.875
+        elif keys[pygame.K_RIGHT]:
+            if p1.y_pos > (stage_y)-(screen_height/10):
+                p1.x_pos = stage_x - p1.x_diameter
+    p1.y_pos += 2
+    p1.hitbox = pygame.Rect(p1.x_pos, p1.y_pos, p1.x_diameter, p1.y_diameter)
+    pygame.draw.rect(win, (255, 0, 0), p1.hitbox)
+    pygame.draw.rect(win, (0, 0, 0), [stage_x, stage_y, screen_width*0.75, screen_height/5])
+    win.blit(pygame.transform.scale(p1_sprite, (p1.x_diameter, p1.y_diameter)), (p1.x_pos, p1.y_pos))
+    win.blit(pygame.transform.scale(p1_weapon, (p1.x_diameter/2, p1.y_diameter)), (p1_weapon_x, p1.y_pos))
+
+    font = pygame.font.SysFont("Times New Roman", screen_width/10)
+    if p1.x_pos <= -p1.x_diameter or p1.x_pos >= screen_width or p1.y_pos <= -p1.y_diameter or p1.y_pos >= screen_height:
         text = font.render("Character died!", True, (0, 0, 0) )
         win.blit(text, ((0), (0)))
+        p1_dead = True
     pygame.display.update()
